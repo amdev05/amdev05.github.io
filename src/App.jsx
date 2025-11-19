58e92d727cc9ac1ba6ede9030498eb247e5007e8
amdev <ademuch80@gmail.com>
2025-11-19 22:21:26 +0700

    update vite & tailwindcss version

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,35 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Profile from "./components/Profile";
-import Technologies from "./components/Technologies";
-import Activity from "./components/Activity";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import MusicPlayer from "./components/MusicPlayer";
+function App() {
+  const [count, setCount] = useState(0)
 
-export default function App() {
   return (
-    <div className="overflow-x-hidden antialiased text-neutral-300">
-      {/* BACKGROUND */}
-      <div className="fixed top-0 -z-10 w-full h-full bg-slate-950">
-        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
-      {/* NAVBAR */}
-      <div className="mx-auto">
-        <Navbar />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
-
-      <div className="">
-        <Hero />
-        {/* <Cursor /> */}
-      </div>
-
-      {/* PAGES */}
-      <div className="container mx-auto px-8">
-        <div className="h-screen w-full"></div>
-        <Projects />
-        <Profile />
-        <Technologies />
-        <Activity />
-        <Contact />
-        <Footer />
-      </div>
-
-      {/* MUSIC PLAYER */}
-      <div className="mx-auto">
-        <MusicPlayer />
-      </div>
-    </div>
-  );
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
